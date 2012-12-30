6a5b87ee04ffe2a2206bd292fc5da87c16405549
kcho <cke8671@hotmail.com>
2012-12-30 11:48:32 -0500

    new version

--- T1andRest.py
+++ T1andRest.py
@@ -9,27 +9,27 @@ rest = re.compile(r'\S*[Rr][Ee][Ss][Tt]\S*')
 
 
 #data source directory
-dataDirectory = os.path.abspath('/volume1/CCNC_MRI/')
+#dataDirectory = os.path.abspath('/volume1/CCNC_MRI')
+dataDirectory = os.path.abspath('/Users/mav88/Dropbox/python/exampleNas')
 
 #groups to copy
 copyGroups=['CHR','NOR']
 
+targetDirectory = os.path.abspath('/Users/mav88/Dropbox/python/nasTarget')
+
 #target directory
-if os.path.isdir('/volume1/2012_12_31') == True:
-    targetDirectory = os.path.abspath('/volume1/2012_12_31')
-else:
-    os.mkdir('/volume1/2012_12_31')
-    targetDirectory = os.path.abspath('/volume1/2012_12_31')
+#if os.path.isdir('/volume1/2012_12_31') == True:
+#    targetDirectory = os.path.abspath('/volume1/2012_12_31')
+#else:
+#    os.mkdir('/volume1/2012_12_31')
+#    targetDirectory = os.path.abspath('/volume1/2012_12_31')
 
 for group in copyGroups:
     subjectList = os.listdir(os.path.join(dataDirectory,group))
     for subject in subjectList:
         subfolder = os.listdir(os.path.join(dataDirectory,group,subject))
-        imageFolders = os.listdir(os.path.join(dataDirectory,group,subject,re.findall('[0-9A-Za-z]',subfolder)))
-        T1 = t1.match(imageFolders)
-        REST = rest.match(imageFolders)
-        print T1.group()
-        print REST.group()
+        rightFolder = re.search('CNT|RESEARCH|2011|2012|2010','subfolder')
+        print rightFolder
 
 
 
