6b5aab7dd99ecd5e555ffc52f5dda00a874299e5
kcho <cke8671@hotmail.com>
2012-12-30 12:00:27 -0500

    up to printing stage

--- T1andRest.py
+++ T1andRest.py
@@ -9,27 +9,31 @@ rest = re.compile(r'\S*[Rr][Ee][Ss][Tt]\S*')
 
 
 #data source directory
-#dataDirectory = os.path.abspath('/volume1/CCNC_MRI')
-dataDirectory = os.path.abspath('/Users/mav88/Dropbox/python/exampleNas')
+dataDirectory = os.path.abspath('/volume1/CCNC_MRI')
+#dataDirectory = os.path.abspath('/Users/mav88/Dropbox/python/exampleNas')
 
 #groups to copy
 copyGroups=['CHR','NOR']
 
-targetDirectory = os.path.abspath('/Users/mav88/Dropbox/python/nasTarget')
+#targetDirectory = os.path.abspath('/Users/mav88/Dropbox/python/nasTarget')
 
 #target directory
-#if os.path.isdir('/volume1/2012_12_31') == True:
-#    targetDirectory = os.path.abspath('/volume1/2012_12_31')
-#else:
-#    os.mkdir('/volume1/2012_12_31')
-#    targetDirectory = os.path.abspath('/volume1/2012_12_31')
+if os.path.isdir('/volume1/2012_12_31') == True:
+    targetDirectory = os.path.abspath('/volume1/2012_12_31')
+else:
+    os.mkdir('/volume1/2012_12_31')
+    targetDirectory = os.path.abspath('/volume1/2012_12_31')
 
 for group in copyGroups:
     subjectList = os.listdir(os.path.join(dataDirectory,group))
     for subject in subjectList:
-        subfolder = os.listdir(os.path.join(dataDirectory,group,subject))
-        rightFolder = re.search('CNT|RESEARCH|2011|2012|2010','subfolder')
-        print rightFolder
+        subfolders = os.listdir(os.path.join(dataDirectory,group,subject))
+        subfolder = re.findall(r'.*CNT.*|.*RESEARCH.*|.*2011.*|.*2012.*|.*2010.*',' '.join(subfolders))
+        imagefolders = os.listdir(os.path.join(dataDirectory,group,subject,''.join(subfolder)))
+        T1 = t1.findall(' '.join(imagefolders))
+        print T1
+        REST = rest.findall(' '.join(imagefolders))
+        print REST
 
 
 
