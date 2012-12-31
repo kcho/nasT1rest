29f1807133b2b8f1cb7d279cd630b7dbcb944df5
kcho <cke8671@hotmail.com>
2012-12-31 11:33:04 -0500

    current working version

--- T1andRest.py
+++ T1andRest.py
@@ -2,13 +2,11 @@
 
 import re
 import os
+import glob
 import shutil
 
-t1 = re.compile(r'TFL\S*|\S*T1\S*|\S*t1\S*')
-rest = re.compile(r'\S*[Rr][Ee][Ss][Tt]\S*')
-
-
-#data source directory
+t1Comp = re.compile(r'TFL\S*|\S*(?<!S)T1\S*|\S*(?<!s)t1\S*')
+restComp = re.compile(r'\S*[Rr][Ee][Ss][Tt]\S*')
 dataDirectory = os.path.abspath('/volume1/CCNC_MRI')
 #dataDirectory = os.path.abspath('/Users/mav88/Dropbox/python/exampleNas')
 
@@ -24,17 +22,63 @@ else:
     os.mkdir('/volume1/2012_12_31')
     targetDirectory = os.path.abspath('/volume1/2012_12_31')
 
+noT1=[]
+noREST=[]
 for group in copyGroups:
-    subjectList = os.listdir(os.path.join(dataDirectory,group))
+    groupDirectory = os.path.join(dataDirectory,group)
+    subjectList = glob.glob(groupDirectory+'/'+ group +'[0-9][0-9]_[A-Z][A-Z][A-Z]')
     for subject in subjectList:
-        subfolders = os.listdir(os.path.join(dataDirectory,group,subject))
-        subfolders = [item for item in subfolders if re.search('\@','item')]
-        print subfolders
-        imagefolders = os.listdir(os.path.join(dataDirectory,group,subject,''.join(subfolders)))
-        T1 = t1.findall(' '.join(imagefolders))
-        print T1
-        REST = rest.findall(' '.join(imagefolders))
-        print REST
+        subjectName = os.path.basename(subject)
+        subfolder = glob.glob(subject+'/[A-Za-z0-9]*')
+        subfolder = [item for item in subfolder if os.path.isdir(item)==True]
+        if len(subfolder) > 1:
+            answer = raw_input('\n\n{0} has more than 1 subfolders\nChoose one between \n1=\t\t{1}\n2=\t\t{2}\n == '.format(
+                    subject,subfolder[0],subfolder[1]))
+            if answer == "1":
+                subfolder = subfolder[0]
+            elif answer =="2":
+                subfolder = subfolder[1]
+            else:
+                print 'this subject is skipped'
+                continue
+
+
+        #imageFolders = os.listdir(os.path.abspath(''.join(subfolder)))
+        imageFolders = glob.glob(''.join(subfolder)+'/[A-Za-z0-9]*')
+        T1Copy=[]
+        RESTCopy=[]
+        for i in imageFolders:
+            T1 = ''.join(t1Comp.findall(i))
+            REST = ''.join(restComp.findall(i))
+            if REST:
+                if len(glob.glob(i+'/*')) > 4000:
+                    os.makedirs(targetDirectory+'/'+subjectName+'/'+'REST')
+                    dest =  os.path.join(targetDirectory,subjectName,'REST')
+                    os.system('cp -r {0} {1}'.format(i,dest))
+
+                    RESTCopy.append(i)
+
+            if T1:
+                if len(glob.glob(i+'/*')) > 208 and len(glob.glob(i+'/*')) < 220:
+                    os.makedirs(targetDirectory+'/'+subjectName+'/'+'T1')
+                    destT =  os.path.join(targetDirectory,subjectName,'T1')
+                    os.system('cp -r {0} {1}'.format(i,destT))
+                    T1Copy.append(i)
+        if T1Copy==[]:
+            print '{} has no T1'.format(subject)
+            noT1.append(subject)
+        if RESTCopy==[]:
+            print '{} has no REST'.format(subject)
+            noREST.append(subject)
+
+
+print noT1,'\n\n\n'
+print noREST
+
+
+
+
+
 
 
 
